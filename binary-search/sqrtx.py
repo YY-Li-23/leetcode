a88cbe20001477d987e0df2fa63621c8e2006011
YY-Li-23 <unixpoolwow@gmail.com>
2025-09-16 20:48:12 +0200

    LC-sqrtx [Runtime: 0 ms; Memory: 17.7 MB]

---
+++ binary-search/sqrtx.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x>=0:
+            t=math.sqrt(x)
+            new_t=math.floor(t)
+        else:
+            False
+        return new_t
+        
