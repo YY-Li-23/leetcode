dc4f42ff133b1f3c444c8941afe8429a1a1224e0
YY-Li-23 <unixpoolwow@gmail.com>
2025-10-04 22:55:22 +0200

    LC-single-number [Runtime: 0 ms; Memory: 19.6 MB]

--- array/single-number.py
+++ array/single-number.py
@@ -1,10 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            t=0
-            for j in range(len(nums)):
-                if nums[i]==nums[j]:
-                    t+=1
-            if t==1:
-                return nums[i]
+        result = 0
+        for n in nums:
+            result ^= n   # XOR: cancels out numbers that appear twice
+        return result
         
