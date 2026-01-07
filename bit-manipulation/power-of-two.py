47e38fda2b3da4f4a0cfdd9aae7c726c6c4fbce8
YY-Li-23 <unixpoolwow@gmail.com>
2026-01-07 21:20:36 +0100

    LC-power-of-two [Runtime: 48 ms; Memory: 19.4 MB]

--- bit-manipulation/power-of-two.py
+++ bit-manipulation/power-of-two.py
@@ -1,9 +1,19 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        i=0
-        while i <n:
-            if 2**i==n:
-                return True
-            else:
-                i+=1
-        return False 
+        if n <=0:
+            return False
+        while n %2 ==0:
+            n//=2
+        return n==1
+
+
+
+
+
+    #    i=0
+    #    while i <n:
+    #       if 2**i==n:
+    #           return True
+    #       else:
+    #           i+=1
+    #   return False 
